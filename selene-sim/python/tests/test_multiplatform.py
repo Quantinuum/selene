4ac465fcc7cad72303709af614e086fd9d8d0f2a
Jake Arkinstall <65358059+jake-arkinstall@users.noreply.github.com>
2026-02-04 10:54:48 +0000

    Add the new gates to instruction log, add pytket output test for them

--- selene-sim/python/tests/test_multiplatform.py
+++ selene-sim/python/tests/test_multiplatform.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import platform
 
 import yaml
+from selene_sim.event_hooks import CircuitExtractor
 from selene_sim import Quest
 from selene_sim.build import build
 from selene_helios_qis_plugin import HeliosInterface
@@ -68,3 +69,49 @@ def test_multiplatform(snapshot, program_name: str):
     assert results["helios"] == results["sol"]
 
     snapshot.assert_match(yaml.dump(results), f"{program_name}.yaml")
+
+
+@pytest.mark.parametrize(
+    "program_name",
+    [
+        "add_3_11",
+    ],
+)
+def test_multiplatform_circuit_log(snapshot, program_name: str):
+    filename = f"{program_name}-{get_platform_suffix()}.ll"
+    helios_file = QIS_RESOURCE_DIR / "helios" / filename
+    sol_file = QIS_RESOURCE_DIR / "sol" / filename
+    assert helios_file.exists()
+    assert sol_file.exists()
+
+    helios_build = build(helios_file, interface=HeliosInterface())
+    sol_build = build(sol_file, interface=SolInterface())
+
+    helios_circuit_extractor = CircuitExtractor()
+    sol_circuit_extractor = CircuitExtractor()
+
+    helios_results = helios_build.run_shots(
+        Quest(),
+        n_qubits=10,
+        n_shots=1,
+        random_seed=1024,
+        event_hook=helios_circuit_extractor,
+    )
+    sol_results = sol_build.run_shots(
+        Quest(),
+        n_qubits=10,
+        n_shots=1,
+        random_seed=1024,
+        event_hook=sol_circuit_extractor,
+    )
+
+    results = {
+        "helios": list(list(shot) for shot in helios_results),
+        "sol": list(list(shot) for shot in sol_results),
+    }
+    circuits = {
+        "helios": repr(helios_circuit_extractor.shots[0].get_user_circuit()),
+        "sol": repr(sol_circuit_extractor.shots[0].get_user_circuit()),
+    }
+
+    snapshot.assert_match(yaml.dump(circuits), f"{program_name}_circuits.yaml")
