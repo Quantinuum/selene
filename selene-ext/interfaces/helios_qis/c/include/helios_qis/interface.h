c4d58ab2828fe6fdc221c90022200921690d8563
Jake Arkinstall <65358059+jake-arkinstall@users.noreply.github.com>
2026-02-12 17:20:29 +0000

    Feat: Test on QIS snapshots (#134)
    Selene primarily provides consumption of QIS (via HUGR and QIR), yet
    does not directly test on QIS snapshots.
    
    Here I add some preliminary QIS tests, in a step towards making QIS a
    first class citizen. The QIS is generated from HUGR, but later I will
    provide some handwritten examples to make the input abundantly clear to
    an interested reader.
    
    This provides us with a starting point for adding additional QIS
    flavours and testing their respective behaviours directly.
    
    Drive-by fixes:
    - Remove commented-out code from emulator.rs, which updated metrics in a
    long-removed way.
    - Added appropriate `_WIN32` guard on defining EXPORT as
    `__declspec(dllimport)` on the helios qis interface C api.

--- selene-ext/interfaces/helios_qis/c/include/helios_qis/interface.h
+++ selene-ext/interfaces/helios_qis/c/include/helios_qis/interface.h
@@ -17,7 +17,11 @@
 // When consuming, we mark the functions as imported on
 // windows. No special marking is needed on other platforms.
 #else
+#ifdef _WIN32
 #define EXPORT __declspec(dllimport)
+#else
+#define EXPORT
+#endif
 #endif
 
 
