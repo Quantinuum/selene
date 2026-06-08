16f20e9111c84b9f29b853b100b1b95d584e92ca
Jake Arkinstall <65358059+jake-arkinstall@users.noreply.github.com>
2026-06-08 15:52:42 +0100

    Update selene.h header

--- selene-sim/c/include/selene/selene.h
+++ selene-sim/c/include/selene/selene.h
@@ -286,7 +286,6 @@ struct selene_u64_result_t selene_shot_count(struct SeleneInstance *instance);
 struct selene_void_result_t selene_simulate_delay(struct SeleneInstance *instance,
                                                   uint64_t delay);
 
-
 /**
  * Read the output stream buffer from the point of the last read, up to a maximum length, copying it into the provided pointer.
  *
